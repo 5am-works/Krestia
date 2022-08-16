ecbe842ad26b645a5637a62d0cd613754d23321c
Sam Wang <samofcorinth@gmx.ca>
2022-08-15 20:58:52 -0700

    Added more words

--- Krestia.Lexicon/Lexicon.cs
+++ Krestia.Lexicon/Lexicon.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections.Immutable;
-
-namespace Krestia.Lexicon;
+﻿namespace Krestia.Lexicon;
 
 public class Lexicon {
    public List<Word> Nouns { get; set; } = new();
-   public List<Word> Verbs { get; set; } = new();
+   public List<Word> AssociativeNouns { get; set; } = new();
+   public List<Verb> Verbs { get; set; } = new();
+   public List<Word> OtherWords { get; set; } = new();
+   public List<List<string>> RelatedWords { get; set; } = new();
 }
 
 public class Word {
-   public string Spelling { get; set; } = "";
-   public string Meaning { get; set; } = "";
-   public string Gloss { get; set; } = "";
+   public string Spelling { get; set; } = null!;
+   public string Meaning { get; set; } = null!;
+   public string? Gloss { get; set; }
    public List<string> Roots { get; set; } = new();
    public string? Remarks { get; set; }
+}
+
+public class Verb : Word {
+   public string Context { get; set; } = null!;
+   public string? ExpandedForm { get; set; }
 }
